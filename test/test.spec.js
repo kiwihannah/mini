d93a3951c86fc93827787e33fecdee61808b00b6
Hannah <gkssk8827@gmail.com>
2022-04-20 17:57:46 +0900

    10

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,20 +1,21 @@
-const assert = require('assert');
-const calc = require('../models/calc')
-suite('[TDD] calculator test', () => {
-    test('add two number test', () => {
-        const ret = calc.plus(1,2);
-        assert.equal(ret, 3);
-    })
-    test('minus two number test', () => {
-        const ret = calc.minus(3,2);
-        assert.equal(ret, 1);
-    })
-    test('multiple two number test', () => {
-        const ret = calc.multi(3,2);
-        assert.equal(ret, 6);
-    })
-    test('division two number test', () => {
-        const ret = calc.divisor(4, 2);
-        assert.equal(ret, 2);
-    })
-})
+const assert = require("assert");
+const calc = require("../models/calc");
+
+suite("[TDD] calc test", () => {
+  test("add two number test", () => {
+    const ret = calc.plus(1, 2);
+    assert.equal(ret, 3);
+  });
+  test("minus two number test", () => {
+    const ret = calc.minus(3, 2);
+    assert.equal(ret, 1);
+  });
+  test("multiple two number test", () => {
+    const ret = calc.multi(3, 2);
+    assert.equal(ret, 6);
+  });
+  test("division two number test", () => {
+    const ret = calc.divisor(4, 2);
+    assert.equal(ret, 2);
+  });
+});
